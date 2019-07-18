036a977a406a9a467b79da34bf46c92a9288d9a8
sujitkrjha <sujit.mca16.du@gmail.com>
2019-07-19 05:16:49 +0530

    Bellmond Ford update

--- bellmond_ford_algo.cpp
+++ bellmond_ford_algo.cpp
@@ -1,20 +1,22 @@
 //Shortest path algorithm
 //Aim to find the shortest distance from source node to every other node
-//Checks for there is no negative weight cycle in the graph 
+//Checks for there is no negative weight cycle in the graph
+
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
 #define pb push_back
 #define mp make_pair
 #define MAX 2000000009
+
 vector<int> graph[55];
-int dist[110]={0},start[110]={0};
-int t,v,e,i,k,st,en,weight;
-int parent[110]={-1};
+int dist[110]={0},parent[110]={-1};
+int v,e;
 bool flag = true;
+
 void initialize(int arr[])
 {
-	for(int j=1;j<=v;++i)
+	for(int j=1;j<v;++j)
 	{
 		dist[j]=MAX;
 	}
@@ -22,24 +24,23 @@ void initialize(int arr[])
 
 void shortestPath(int a, int b)
 {
-	if(dist[b] > dist[a] + graph[a][1])
+	if(dist[b] > dist[a] + graph[a][2])
 	{
 		flag = true;
-		dist[b] = dist[a] + graph[a][1];
+		dist[b] = dist[a] + graph[a][2];
 		parent[b] = a;
 	}
 }
 
 int main()
 {
-	
+	int t,i,k,st,en,weight;
 	cin>>t;
 	while(t--)
 	{
-		memset(start, 0 ,110*4);
 		flag = true;
-		initialize(dist);
 		cin>>v>>e;
+		initialize(dist);
 		for(i=0;i<e;++i)
 		{
 			scanf("%d %d %d ",&st,&en,&weight);
@@ -47,25 +48,24 @@ int main()
 			graph[i].pb(en);
 			graph[i].pb(weight);
 		}
-		cout<<"here"<<endl;
 		for(i=0;i<v;++i)
 		{
 			for(k=0;k<e;++k)
 			{
 				shortestPath(graph[k][0], graph[k][1]);
 			}
-			if(i==v-1)
+			if(i==v-2)
 			{
 				flag = false;
 			}
 		}
-		cout<<"herenow"<<endl;
 		if(flag == true)
 			cout<<"1"<<endl;
 		else
 			cout<<"0"<<endl;
 		for(i=0;i<e;++i)
 			graph[i].clear();
+		dist[0]=0;
 	}
 	return 0;
 }
