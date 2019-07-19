3fc853b08fcfdbe17ab7202b582b4b932e8dbbc3
sujitkrjha <sujit.mca16.du@gmail.com>
2019-07-19 05:44:09 +0530

    Max Priority Queue

---
+++ max_priority_queue.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+priority_queue <int> pq;
+
+int main(){
+    pq.push(5);
+    pq.push(15);
+    pq.push(3);
+    
+    while (pq.empty() == false) 
+    { 
+        cout << pq.top() << " "; 
+        pq.pop(); 
+    } 
+  
+    return 0;
+}
